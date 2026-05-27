078846c51f7fb1cdb899b32fe1527f50a3e5a6d7
s3w3ll <Hayden.sewell@gmail.com>
2026-05-27 13:08:36 +1200

    alerts issue

--- workers/uv-notifier/.wrangler/tmp/dev-N2NaYE/index.js
+++ workers/uv-notifier/.wrangler/tmp/dev-N2NaYE/index.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-fR4dPq/checked-fetch.js
+// .wrangler/tmp/bundle-iU9VjA/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -321,7 +321,7 @@ var scheduled = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "scheduled");
 var middleware_scheduled_default = scheduled;
 
-// .wrangler/tmp/bundle-fR4dPq/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-iU9VjA/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_scheduled_default
@@ -353,7 +353,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-fR4dPq/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-iU9VjA/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
